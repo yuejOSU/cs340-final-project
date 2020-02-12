39f457842caac79c53ac6cc4fe30ae7278a126af
Andy Trinh <trinha@flip1.engr.oregonstate.edu>
2020-02-12 14:09:35 -0800

    create tables

---
+++ hotelserver/tables/create.sql
@@ -0,0 +1,35 @@
+-- Customers
+CREATE TABLE `Customers` (
+`customer_id` int(11) AUTO_INCREMENT NOT NULL,
+`email_address`, varchar(255), NOT NULL,
+`first_name` varchar(255) NOT NULL,
+`last_name` varchar(255) NOT NULL,
+PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Bookings
+CREATE TABLE `Bookings` (
+`booking_id` int(11) AUTO_INCREMENT NOT NULL,
+`cid` int,
+`booking_date` varchar(255), NOT NULL
+FOREIGN KEY (cid) REFERENCES Customers(customer_id),
+PRIMARY KEY (`booking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- Booking_Details
+CREATE TABLE `Booking_Details` (
+`bid` int,
+`rid` int,
+`booking_price` int, NOT NULL
+FOREIGN KEY (bid) REFERENCES Bookings(booking_id),
+FOREIGN KEY (rid) REFERENCES Rooms(room_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Rooms
+CREATE TABLE `Rooms` (
+`room_id` int NOT NULL,
+`room_price` int NOT NULL,
+`is_clean` boolean NOT NULL,
+`is_occupied` boolean NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
