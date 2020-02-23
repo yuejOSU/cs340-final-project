da56fc621577567f88577cf23a4070bdfc65bb8f
yuejOSU <yuej@oregonstate.edu>
2020-02-23 14:55:38 -0800

    fix ddq_file to work as import dump, fix queries in JS, change email to email_address on handlebars for render

--- hotelserver/hotelserver.js
+++ hotelserver/hotelserver.js
@@ -21,110 +21,110 @@ app.use(express.static(path.join(__dirname, '/public')));
 var x = 0;
 
 app.get('/',function(req,res,next){
-  var context = {};
+  // var context = {};
 
-  var createTables = [
-  "CREATE TABLE IF NOT EXISTS `customers`(" +
-  "`customer_id` INT(11) PRIMARY KEY AUTO_INCREMENT," +
-  "`first_name` VARCHAR(255) NOT NULL," +
-  "`last_name` VARCHAR(255) NOT NULL," +
-  "`email` VARCHAR(255) NOT NULL," +
-  "`age` INT(11) NOT NULL);",
+  // var createTables = [
+  // "CREATE TABLE IF NOT EXISTS `customers`(" +
+  // "`customer_id` INT(11) PRIMARY KEY AUTO_INCREMENT," +
+  // "`first_name` VARCHAR(255) NOT NULL," +
+  // "`last_name` VARCHAR(255) NOT NULL," +
+  // "`email` VARCHAR(255) NOT NULL," +
+  // "`age` INT(11) NOT NULL);",
 
-  "CREATE TABLE IF NOT EXISTS `bookings`(" +
-  "`booking_id` INT(11) PRIMARY KEY AUTO_INCREMENT," +
-  "`cid` INT(11)," +
-  "`booking_date` date NOT NULL," +
-  "FOREIGN KEY (cid) REFERENCES customers(customer_id));",
+  // "CREATE TABLE IF NOT EXISTS `bookings`(" +
+  // "`booking_id` INT(11) PRIMARY KEY AUTO_INCREMENT," +
+  // "`cid` INT(11)," +
+  // "`booking_date` date NOT NULL," +
+  // "FOREIGN KEY (cid) REFERENCES customers(customer_id));",
 
-  "CREATE TABLE IF NOT EXISTS `rooms`(" +
-  "`room_id` INT(11) AUTO_INCREMENT," +
-  "`is_clean` boolean NOT NULL," +
-  "`is_occupied` boolean NOT NULL," +
-  "PRIMARY KEY (`room_id`));",
+  // "CREATE TABLE IF NOT EXISTS `rooms`(" +
+  // "`room_id` INT(11) AUTO_INCREMENT," +
+  // "`is_clean` boolean NOT NULL," +
+  // "`is_occupied` boolean NOT NULL," +
+  // "PRIMARY KEY (`room_id`));",
 
-  "CREATE TABLE IF NOT EXISTS `booking_details`(" +
-  "`booking_details_id` INT(11) PRIMARY KEY AUTO_INCREMENT," +
-  "`bid` INT(11)," +
-  "`rid` INT(11)," +
-  "`booking_price` INT NOT NULL," +
-  "FOREIGN KEY (bid) REFERENCES customers(customer_id)," +
-  "FOREIGN KEY (rid) REFERENCES rooms(room_id));"
-  ];
+  // "CREATE TABLE IF NOT EXISTS `booking_details`(" +
+  // "`booking_details_id` INT(11) PRIMARY KEY AUTO_INCREMENT," +
+  // "`bid` INT(11)," +
+  // "`rid` INT(11)," +
+  // "`booking_price` INT NOT NULL," +
+  // "FOREIGN KEY (bid) REFERENCES customers(customer_id)," +
+  // "FOREIGN KEY (rid) REFERENCES rooms(room_id));"
+  // ];
 
-  var deleteTables = [
-  "DROP TABLE IF EXISTS `customers`",
-  "DROP TABLE IF EXISTS `bookings`",
-  "DROP TABLE IF EXISTS `rooms`",
-  "DROP TABLE IF EXISTS `booking_details`"
-  ];
+  // var deleteTables = [
+  // "DROP TABLE IF EXISTS `customers`",
+  // "DROP TABLE IF EXISTS `bookings`",
+  // "DROP TABLE IF EXISTS `rooms`",
+  // "DROP TABLE IF EXISTS `booking_details`"
+  // ];
 
-  var insert = [
-  "INSERT INTO `customers` (`email`, `first_name`, `last_name`, `age`) VALUES (\"boobroos@gmail.com\", \"Bob\", \"Ross\", 27)",
-  "INSERT INTO `bookings` (`booking_date`) VALUES (\'1920-09-20\')",
-  "INSERT INTO `rooms` (`is_clean`, `is_occupied`) VALUES (false, true)",
-  "INSERT INTO `booking_details` (`booking_price`) VALUES (1)"
-  ];
+  // var insert = [
+  // "INSERT INTO `customers` (`email`, `first_name`, `last_name`, `age`) VALUES (\"boobroos@gmail.com\", \"Bob\", \"Ross\", 27)",
+  // "INSERT INTO `bookings` (`booking_date`) VALUES (\'1920-09-20\')",
+  // "INSERT INTO `rooms` (`is_clean`, `is_occupied`) VALUES (false, true)",
+  // "INSERT INTO `booking_details` (`booking_price`) VALUES (1)"
+  // ];
 
 
-  if(!x) {
-    for (i = 0; i < 4; i++) {
-    //  mysql.pool.query(deleteTables[i], function(err){
-    //    if(err){
-    //      next(err);
-    //      return;
-    //    }
-    //  });
-      mysql.pool.query(createTables[i], function(err){
-        if(err){
-          next(err);
-          return;
-        }
-      });
-      mysql.pool.query(insert[i], function(err){
-        if(err){
-          next(err);
-          return;
-        }
-      });
-    };
-  }
-  x = 1;
+  // if(!x) {
+  //   for (i = 0; i < 4; i++) {
+  //   //  mysql.pool.query(deleteTables[i], function(err){
+  //   //    if(err){
+  //   //      next(err);
+  //   //      return;
+  //   //    }
+  //   //  });
+  //     mysql.pool.query(createTables[i], function(err){
+  //       if(err){
+  //         next(err);
+  //         return;
+  //       }
+  //     });
+  //     mysql.pool.query(insert[i], function(err){
+  //       if(err){
+  //         next(err);
+  //         return;
+  //       }
+  //     });
+  //   };
+  // }
+  // x = 1;
 
   res.render('home');
 });
 
 app.get('/create-customer-account',function(req,res,next){
   var context = {};
-    mysql.pool.query('SELECT * FROM `customers`', function(err, rows, fields){
-      context.results = rows;
-	  res.render('create-customer-account',context);
-	  });
+  mysql.pool.query('SELECT * FROM `Customers`', function(err, rows, fields){
+    context.results = rows;
+    res.render('create-customer-account',context);
+  });
 });
 
 app.post('/create-customer-account', function(req, res, next){
   var context = {};
   var params  = req.body;
-    mysql.pool.query('INSERT INTO customers SET ?', params, function(err, results, fields){
-    mysql.pool.query('SELECT * FROM `customers`', function(err, rows, fields){
-      context.results = rows;
-	    res.render('create-customer-account',context);
-	  });
+    mysql.pool.query('INSERT INTO Customers SET ?', params, function(err, results, fields){
+      mysql.pool.query('SELECT * FROM `Customers`', function(err, rows, fields){
+        context.results = rows;
+        res.render('create-customer-account',context);
+      });
   });
 });
 
 
 app.get('/search-customer',function(req,res,next){
   var context = {};
-    mysql.pool.query('SELECT first_name, last_name, email, age FROM `customers`', function(err, rows, fields){
+  mysql.pool.query('SELECT first_name, last_name, email_address, age FROM `customers`', function(err, rows, fields){
     context.results = rows;
-	  res.render('search-customer',context);
-	  });
+    res.render('search-customer',context);
+  });
 });
 
 app.get('/create-booking',function(req,res,next){
   var context = {};
-    mysql.pool.query('SELECT * FROM `bookings`', function(err, rows, fields){
+    mysql.pool.query('SELECT * FROM `Bookings`', function(err, rows, fields){
       context.results = rows;
 	  res.render('create-booking',context);
 	  });
@@ -133,8 +133,8 @@ app.get('/create-booking',function(req,res,next){
 app.post('/create-booking', function(req, res, next){
   var context = {};
   var params  = req.body;
-    mysql.pool.query('INSERT INTO bookings SET ?', params, function(err, results, fields){
-    mysql.pool.query('SELECT * FROM `bookings`', function(err, rows, fields){
+    mysql.pool.query('INSERT INTO Bookings SET ?', params, function(err, results, fields){
+    mysql.pool.query('SELECT * FROM `Bookings`', function(err, rows, fields){
       context.results = rows;
 	    res.render('create-booking',context);
 	  });
@@ -143,7 +143,7 @@ app.post('/create-booking', function(req, res, next){
 
 app.get('/search-booking-details',function(req,res,next){
   var context = {};
-  mysql.pool.query('SELECT * FROM `booking_details`', function(err, rows, fields){
+  mysql.pool.query('SELECT * FROM `Booking_Details`', function(err, rows, fields){
     context.results = rows;
     res.render('search-booking-details',context);
   });
@@ -151,7 +151,7 @@ app.get('/search-booking-details',function(req,res,next){
 
 app.get('/search-existing-rooms',function(req,res,next){
   var context = {};
-    mysql.pool.query('SELECT * FROM `rooms`', function(err, rows, fields){
+    mysql.pool.query('SELECT * FROM `Rooms`', function(err, rows, fields){
       context.results = rows;
 	  res.render('search-existing-rooms',context);
 	  });
@@ -159,7 +159,7 @@ app.get('/search-existing-rooms',function(req,res,next){
 
 app.get('/add-new-rooms',function(req,res,next){
   var context = {};
-    mysql.pool.query('SELECT * FROM `rooms`', function(err, rows, fields){
+    mysql.pool.query('SELECT * FROM `Rooms`', function(err, rows, fields){
       context.results = rows;
 	  res.render('add-new-rooms',context);
 	  });
--- hotelserver/tables/hotelserver_ddq_file.sql
+++ hotelserver/tables/hotelserver_ddq_file.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `Customers`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Customers` (
   `customer_id` int(11) AUTO_INCREMENT NOT NULL,
-  `email_address`, varchar(255), NOT NULL,
+  `email_address` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `age` INT(11) NOT NULL,
@@ -36,9 +36,9 @@ CREATE TABLE `Customers` (
 --
 LOCK TABLES `Customers` WRITE;
 /*!40000 ALTER TABLE `Customers` DISABLE KEYS */;
-  INSERT INTO `customers` VALUES ('boobroos@gmail.com', 'Bob', 'Ross', 27),
-                                 ('jjwattoffense@aol.com', 'Jay Jay', 'Watt', 99),
-                                 ('ballislife@yahoo.com', 'Pro', 'Baller', 18);
+  INSERT INTO `Customers` (`email_address`, `first_name`, `last_name`, `age`) VALUES ('boobroos@gmail.com', 'Bob', 'Ross', 27),
+                                                                                     ('jjwattoffense@aol.com', 'Jay Jay', 'Watt', 99),
+                                                                                     ('ballislife@yahoo.com', 'Pro', 'Baller', 18);
 /*!40000 ALTER TABLE `Customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,17 +50,17 @@ DROP TABLE IF EXISTS `Bookings`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Bookings` (
   `booking_id` int(11) AUTO_INCREMENT NOT NULL,
-  `cid` int,
-  `booking_date` varchar(255), NOT NULL
+  `cid` int NOT NULL DEFAULT '0',
+  `booking_date` date NOT NULL,
   PRIMARY KEY (`booking_id`),
   CONSTRAINT FOREIGN KEY (`cid`) REFERENCES Customers(`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `Bookings` WRITE;
 /*!40000 ALTER TABLE `Bookings` DISABLE KEYS */;
-INSERT INTO `Bookings` VALUES ('1920-09-08'),
-                              ('2020-02-20'),
-                              ('2002-04-20');
+INSERT INTO `Bookings` (`booking_date`) VALUES ('1920-09-08'),
+                                               ('2020-02-20'),
+                                               ('2002-04-20');
 /*!40000 ALTER TABLE `Bookings` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,7 +71,7 @@ DROP TABLE IF EXISTS `Rooms`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Rooms` (
-  `room_id` int NOT NULL,
+  `room_id` int(11) AUTO_INCREMENT NOT NULL,
   `room_price` int NOT NULL,
   `is_clean` boolean NOT NULL,
   `is_occupied` boolean NOT NULL,
@@ -80,9 +80,9 @@ CREATE TABLE `Rooms` (
 
 LOCK TABLES `Rooms` WRITE;
 /*!40000 ALTER TABLE `Rooms` DISABLE KEYS */;
-INSERT INTO `Rooms` VALUES (98, false, true),
-                           (198, false, false),
-                           (299, true, true);
+INSERT INTO `Rooms` (`room_price`, `is_clean`, `is_occupied`) VALUES (98, false, true),
+                                                                     (198, false, false),
+                                                                     (299, true, true);
 /*!40000 ALTER TABLE `Rooms` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +96,7 @@ CREATE TABLE `Booking_Details` (
   `booking_details_id` int(11) AUTO_INCREMENT NOT NULL,
   `bid` int NOT NULL DEFAULT '0',
   `rid` int NOT NULL DEFAULT '0',
-  `booking_price` int, NOT NULL
+  `booking_price` int NOT NULL,
   PRIMARY KEY (`booking_details_id`),
   CONSTRAINT FOREIGN KEY (`bid`) REFERENCES Bookings(`booking_id`),
   CONSTRAINT FOREIGN KEY (`rid`) REFERENCES Rooms(`room_id`)
@@ -104,9 +104,9 @@ CREATE TABLE `Booking_Details` (
 
 LOCK TABLES `Booking_Details` WRITE;
 /*!40000 ALTER TABLE `Booking_Details` DISABLE KEYS */;
-INSERT INTO `booking_details` VALUES (296),
-                                     (497),
-                                     (397);
+INSERT INTO `Booking_Details` (`booking_price`) VALUES (296),
+                                                       (497),
+                                                       (397);
 /*!40000 ALTER TABLE `Booking_Details` ENABLE KEYS */;
 UNLOCK TABLES;
 
